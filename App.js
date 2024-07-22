5687227c4fbf0fc0d441b55433e3053be013816a
Nayna <shevkarinayna@gmail.com>
2024-07-22 15:16:18 -0400

    Updated App

--- App.js
+++ App.js
@@ -1,3 +1,38 @@
+// import "dotenv/config";
+// import express from 'express';
+// import mongoose from "mongoose";
+// import UserRoutes from "./User/routes.js";
+// import Hello from "./Hello.js";
+// import Lab5 from "./Lab5/index.js";
+// import CourseRoutes from './Kanbas/Courses/routes.js';
+// import ModuleRoutes from './Kanbas/Modules/routes.js';
+// import cors from "cors";
+// import AssignmentsRoutes from './Kanbas/Courses/Assignments/routes.js';
+
+// const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+// // mongoose.connect(CONNECTION_STRING);
+
+// mongoose.connect(CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => {
+//     console.log('MongoDB connected');
+//   })
+//   .catch(err => {
+//     console.error('MongoDB connection error:', err);
+//   });
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+// UserRoutes(app);
+// CourseRoutes(app);
+// ModuleRoutes(app);
+// AssignmentsRoutes(app);
+// Lab5(app);
+// Hello(app);
+// app.listen(process.env.PORT || 4000);
+
+
+
 import "dotenv/config";
 import express from 'express';
 import mongoose from "mongoose";
@@ -9,19 +44,16 @@ import ModuleRoutes from './Kanbas/Modules/routes.js';
 import cors from "cors";
 import AssignmentsRoutes from './Kanbas/Courses/Assignments/routes.js';
 
-const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
-mongoose.connect(CONNECTION_STRING);
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
 
-// mongoose.connect(CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true });
-// const db = mongoose.connection;
-
-// db.once('open', _ => {
-//   console.log('Database connected:', CONNECTION_STRING);
-// });
-
-// db.on('error', err => {
-//   console.error('Connection error:', err);
-// });
+mongoose.connect(CONNECTION_STRING, {
+})
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+  });
 
 const app = express();
 app.use(cors());
@@ -33,8 +65,3 @@ AssignmentsRoutes(app);
 Lab5(app);
 Hello(app);
 app.listen(process.env.PORT || 4000);
-// const PORT = process.env.PORT || 4000;
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-//   });
-
