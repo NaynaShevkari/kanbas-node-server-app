d73f73081a3f2197aeb5e12f7328344afbe7938a
Nayna <shevkarinayna@gmail.com>
2024-07-21 23:44:55 -0400

    A6 first

--- App.js
+++ App.js
@@ -1,16 +1,40 @@
-import express from 'express'
+import "dotenv/config";
+import express from 'express';
+import mongoose from "mongoose";
+import UserRoutes from "./User/routes.js";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import CourseRoutes from './Kanbas/Courses/routes.js';
 import ModuleRoutes from './Kanbas/Modules/routes.js';
 import cors from "cors";
 import AssignmentsRoutes from './Kanbas/Courses/Assignments/routes.js';
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
+
+// mongoose.connect(CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true });
+// const db = mongoose.connection;
+
+// db.once('open', _ => {
+//   console.log('Database connected:', CONNECTION_STRING);
+// });
+
+// db.on('error', err => {
+//   console.error('Connection error:', err);
+// });
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentsRoutes(app);
 Lab5(app);
 Hello(app);
-app.listen(process.env.PORT || 4000)
+app.listen(process.env.PORT || 4000);
+// const PORT = process.env.PORT || 4000;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+//   });
+
